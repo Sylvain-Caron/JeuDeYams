0bcaaabdc49dc4fa50796d6b91d2b31ad80287d2
Florian <florianchanchus83@gmail.com>
2022-04-28 11:16:44 +0200

    update socre calcul

--- src/app/score-calcul.service.ts
+++ src/app/score-calcul.service.ts
@@ -8,7 +8,7 @@ import { Joueur } from './joueur/Joueur';
 export class ScoreCalculService {
 
   constructor() { }
-
+  
   getValeur(listDice : Dice[]) {
     let listValeurDice = []
     for(var dice of listDice){ 
@@ -19,15 +19,20 @@ export class ScoreCalculService {
 
   calculScore(listDice : Dice[], leJoueur : Joueur) {
     let listValeurDice = this.getValeur(listDice)
+    let dicValeur : any = {}
     console.log("Dans la fonction calculScore avec la liste des dès et le Joueur : " + leJoueur)
-    for(var dice of listDice){
+    for(var dice of listValeurDice){
       console.log(dice) 
+      if (dicValeur[dice]) {
+        dicValeur[dice] += 1;
+      } else {
+        dicValeur[dice] = 1;
+      }
     }
-    console.log(listValeurDice)
+    console.log(dicValeur)
     console.log("Joueur : " + leJoueur.nom)
 
     // Avec la liste des dès on peut faire nos calculs 
   }
-
   
 }
