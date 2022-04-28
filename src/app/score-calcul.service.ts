24a8bcdd5559be10638ed15bd4ed84bb7ea6954d
Sylvain <c84.sylvain@gmail.com>
2022-04-28 10:27:33 +0200

    Ajout function score

--- src/app/score-calcul.service.ts
+++ src/app/score-calcul.service.ts
@@ -9,13 +9,25 @@ export class ScoreCalculService {
 
   constructor() { }
 
+  getValeur(listDice : Dice[]) {
+    let listValeurDice = []
+    for(var dice of listDice){ 
+      listValeurDice.push(dice.valeur)
+    }
+    return listValeurDice
+  }
+
   calculScore(listDice : Dice[], leJoueur : Joueur) {
+    let listValeurDice = this.getValeur(listDice)
     console.log("Dans la fonction calculScore avec la liste des dès et le Joueur : " + leJoueur)
     for(var dice of listDice){
-      console.log(dice)
+      console.log(dice) 
     }
+    console.log(listValeurDice)
     console.log("Joueur : " + leJoueur.nom)
 
     // Avec la liste des dès on peut faire nos calculs 
   }
+
+  
 }
